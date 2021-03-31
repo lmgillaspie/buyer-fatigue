2281d134f56ad94a363e6f519f5e2f7a0bade81f
lmgillaspie <lindsey.gillaspie>
2021-03-31 00:12:27 -0400

    Adds gradle node plugin to bundle frontend and backend to deploy

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,10 +1,12 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import com.moowork.gradle.node.npm.NpmTask
 
 plugins {
 	id("org.springframework.boot") version "2.5.0-SNAPSHOT"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
 	kotlin("jvm") version "1.4.31"
 	kotlin("plugin.spring") version "1.4.31"
+	id("com.github.node-gradle.node") version "2.2.2"
 }
 
 group = "lmgillaspie"
@@ -33,7 +35,39 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
+tasks.register<NpmTask>("appNpmInstall") {
+	description = "Installs all dependencies from package.json"
+	workingDir = file("../frontend")
+	args = listOf("install")
+}
+
+tasks.register<NpmTask>("appNpmBuild") {
+	dependsOn("appNpmInstall")
+	description = "Builds project"
+	workingDir = file("../frontend")
+	args = listOf("run", "build")
+}
+
+tasks.register<Copy>("copyWebApp") {
+	dependsOn("appNpmBuild")
+	description = "Copies built project to where it will be served"
+	from("../frontend/build")
+	into("build/resources/main/static/.")
+}
+
+node {
+	download = true
+	version = "12.13.1"
+	npmVersion = "6.12.1"
+	// Set the work directory for unpacking node
+	workDir = file("../../frontend/nodejs")
+	// Set the work directory for NPM
+	npmWorkDir = file("../../frontend/npm")
+}
+
 tasks.withType<KotlinCompile> {
+	// So that all the tasks run with ./gradlew build
+	dependsOn("copyWebApp")
 	kotlinOptions {
 		freeCompilerArgs = listOf("-Xjsr305=strict")
 		jvmTarget = "11"
