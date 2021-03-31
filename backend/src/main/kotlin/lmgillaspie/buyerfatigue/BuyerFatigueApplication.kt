d7f76cb22dd8321c94cade705042d4358902ccd3
lmgillaspie <lindsey.gillaspie>
2021-03-30 23:33:21 -0400

    Spring Initialzr Kotlin

---
+++ backend/src/main/kotlin/lmgillaspie/buyerfatigue/BuyerFatigueApplication.kt
@@ -0,0 +1,11 @@
+package lmgillaspie.buyerfatigue
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BuyerFatigueApplication
+
+fun main(args: Array<String>) {
+	runApplication<BuyerFatigueApplication>(*args)
+}
---
+++ backend/src/test/kotlin/lmgillaspie/buyerfatigue/BuyerFatigueApplicationTests.kt
@@ -0,0 +1,13 @@
+package lmgillaspie.buyerfatigue
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BuyerFatigueApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
